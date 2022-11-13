7751ff2899028acbe7b58417cb6b1e3eb973ad04
Vadim Fevralev <fevralev.vadim@mail.ru>
2022-11-13 20:39:27 +0300

    научились приветствовать по особенному при совпадении имени

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Татьяна")
+{
+    Console.WriteLine("А ты знаешь,как я тебя люблю?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
